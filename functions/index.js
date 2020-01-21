855a133b9e0ff1e209dea98bf1e8c3ecf998f620
Logan Hope Koch <logan.koch@hidigital.solutions>
2020-01-20 19:36:25 -0700

    add trello package and update nvm to correct version

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.getBoards = functions.https.onCall((data, context) => {
+ const Trello = require("trello");
+ const api_key = functions.config().trello.api_key;
+ const access_key = functions.config().trello.access_key;
+ const trello = new Trello(api_key, access_key);
+});
