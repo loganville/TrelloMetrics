8370f923fe52b006a02164770306096013fe0be5
Logan Hope Koch <logan.koch@hidigital.solutions>
2020-01-20 20:56:36 -0700

    move to request promise and get all cards off of a specific board

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,35 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const rp = require("request-promise-native");
+const api_key = functions.config().trello.api_key;
+const access_key = functions.config().trello.access_key;
+const board = functions.config().trello.board;
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
 });
 
-exports.getBoards = functions.https.onCall((data, context) => {
- const Trello = require("trello");
- const api_key = functions.config().trello.api_key;
- const access_key = functions.config().trello.access_key;
- const trello = new Trello(api_key, access_key);
+exports.getCards = functions.https.onRequest((request, response) => {
+    const options = {
+        uri: 'https://api.trello.com/1/boards/' + board + '/cards',
+        qs: {
+            fields: 'name',
+            key: api_key,
+            token: access_key,
+        },
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    };
+
+    rp(options)
+        .then(function (cards) {
+            response.status(200).send(cards);
+            return cards;
+        })
+        .catch(function (err) {
+            response.status(500).send(err);
+            return err;
+        });
 });
