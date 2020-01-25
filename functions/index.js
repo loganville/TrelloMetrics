8e07e42fe94b89e7cb5b28c14893331a07f27b04
Logan Hope Koch <logan.koch@hidigital.solutions>
2020-01-24 23:47:19 -0700

    add cycle time and defect rate

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const api_key = functions.config().trello.api_key;
 const access_key = functions.config().trello.access_key;
 const board = functions.config().trello.board;
 const list = functions.config().trello.list;
+const week_count = 11;
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -82,14 +83,60 @@ exports.updateData = functions.pubsub.schedule('every 23 hours').onRun((context)
 });
 
 exports.defectRate = functions.https.onRequest((request, response) => {
-    db.collection("cards")
-        .where("defect", "==", true)
-        .where( "time_in", "<", moment().subtract(0, 'weeks'))
-        .where( "time_in", ">", moment().subtract(5, 'weeks'))
-        .get()
-        .then(snap => {
-            response.status(200).send({length: snap.size});
-        });
+    let promises = [];
+    for (let week = week_count; week >= 0; week--) {
+        promises.push(new Promise((resolve) => {
+            return db.collection("cards")
+                .where("defect", "==", true)
+                .where("time_in", ">", moment().startOf('isoWeek').subtract(week, 'weeks'))
+                .where("time_in", "<", moment().endOf('isoWeek').subtract(week, 'weeks'))
+                .get()
+                .then(cards => {
+                    resolve(cards.size);
+                });
+        }));
+    }
+    Promise.all(promises).then(function (values) {
+        response.set('Access-Control-Allow-Origin', '*');
+        response.status(200).send(values);
+        return values;
+    });
+});
+
+exports.cycleTime = functions.https.onRequest((request, response) => {
+    let promises = [];
+    for (let week = week_count; week >= 0; week--) {
+        promises.push(new Promise((resolve) => {
+            return db.collection("cards")
+                .where("time_in", ">", moment().startOf('isoWeek').subtract(week, 'weeks'))
+                .where("time_in", "<", moment().endOf('isoWeek').subtract(week, 'weeks'))
+                .get()
+                .then(cards => {
+                    let days = 0;
+                    cards.forEach((card)=>{
+                        const start = moment(card.data().time_in);
+                        const end = moment(card.data().time_out);
+                        days += end.diff(start, 'days', true);
+                    });
+                    resolve(Number.parseFloat((days/Math.max(cards.size,1)).toFixed(2)));
+                });
+        }));
+    }
+    Promise.all(promises).then(function (values) {
+        response.set('Access-Control-Allow-Origin', '*');
+        response.status(200).send(values);
+        return values;
+    });
+});
+
+exports.getCategories = functions.https.onRequest((request, response) => {
+    let results = [];
+    for (let week = week_count; week >= 0; week--) {
+        results.push(moment().startOf('isoWeek').subtract(week, 'weeks').format('MMM DD'));
+    }
+    response.set('Access-Control-Allow-Origin', '*');
+    response.status(200).send(results);
+    return results;
 });
 
 exports.checkLabel = functions.firestore.document('cards/{cardId}').onWrite((change, context) => {
